0d530bc7c572761775dbc05c74e5ac35db3f60e4
Voyant Cyril <59060802+cyrilvoyant@users.noreply.github.com>
2026-06-26 08:52:49 +0200

    Fix import path for simulator module

--- src/tunnel_load_simulator/app.py
+++ src/tunnel_load_simulator/app.py
@@ -23,7 +23,7 @@ import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
 
-from simulator import (
+from tunnel_load_simulator.simulator import (
     CONTEXT_MULT,
     LIGHTING_PARAMS,
     VENTILATION_PARAMS,
